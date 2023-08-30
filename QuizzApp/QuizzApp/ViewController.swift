327c802fb9a89e850f581d0be7615af443aa65a2
David Tigre Moraes <tigredavid007@gmail.com>
2023-08-30 18:05:14 -0300

    Telas iniciais App - WIP

--- QuizzApp/QuizzApp/ViewController.swift
+++ QuizzApp/QuizzApp/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var botaoQuizz: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configuraLayout()
     }
-
-
+    
+    @IBAction func BotaoPressionado(_ sender: Any) {
+        print("OLA MUNDO")
+    }
+    
+    func configuraLayout() {
+        botaoQuizz.layer.cornerRadius = 12.0
+    }
 }
 
